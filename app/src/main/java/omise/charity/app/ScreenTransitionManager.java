37344e4e9348e8736e17b47065e333c4e00a8129
Nami <nami.salimi@hotelquickly.com>
2018-01-25 23:05:54 +0700

    Created success screen and handled its navigation

--- app/src/main/java/omise/charity/app/ScreenTransitionManager.java
+++ app/src/main/java/omise/charity/app/ScreenTransitionManager.java
@@ -10,4 +10,8 @@ public class ScreenTransitionManager {
 	public static void setActivitySlideRightIn(Activity activity) {
 		activity.overridePendingTransition(R.anim.slide_in_enter, R.anim.slide_in_exit);
 	}
+
+	public static void setActivityZoomOut(Activity activity) {
+		activity.overridePendingTransition(android.R.anim.fade_in, R.anim.zoom_out);
+	}
 }
--- app/src/main/java/omise/charity/app/charity/charityDetail/DonationSuccessActivity.java
+++ app/src/main/java/omise/charity/app/charity/charityDetail/DonationSuccessActivity.java
@@ -1,13 +1,37 @@
 package omise.charity.app.charity.charityDetail;
 
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import omise.charity.app.R;
+import omise.charity.app.ScreenTransitionManager;
 
 public class DonationSuccessActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.donation_success_layout);
+		Button dismissButton = findViewById(R.id.donation_success_dismiss_button);
+		ImageView mCheckImageView = findViewById(R.id.donation_success_check_mark);
+
+		((Animatable) mCheckImageView.getDrawable()).start();
+
+		dismissButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				finishScreen();
+			}
+		});
+	}
+
+	private void finishScreen() {
+		ScreenTransitionManager.setActivityZoomOut(this);
+		finish();
 	}
 }
