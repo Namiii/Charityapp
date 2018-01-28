9833d25d665bd13b0ce9f5ec5f09da461613fb6a
Nami <nami.salimi@hotelquickly.com>
2018-01-28 12:13:29 +0700

    Integrated an interceptor for retrofit client

--- app/src/main/java/omise/charity/app/dagger/NetworkModule.java
+++ app/src/main/java/omise/charity/app/dagger/NetworkModule.java
@@ -4,6 +4,8 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import omise.charity.app.Constants;
 import omise.charity.app.charity.CharityDataService;
 import retrofit2.Retrofit;
@@ -11,12 +13,21 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class NetworkModule {
+	@Singleton
+	@Provides
+	OkHttpClient providesHttpClient() {
+		OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+		httpClient.addInterceptor(new HttpLoggingInterceptor()
+				.setLevel(HttpLoggingInterceptor.Level.BODY));
+		return httpClient.build();
+	}
 
 	@Provides
 	@Singleton
-	Retrofit providesRetrofitClient() {
+	Retrofit providesRetrofitClient(OkHttpClient client) {
 		return new Retrofit.Builder()
 				.baseUrl(Constants.BASE_URL)
+				.client(client)
 				.addConverterFactory(GsonConverterFactory.create())
 				.build();
 	}
