f8591f9bbeda38ff89d498ca517cec6d965fcb94
Nami <nami.salimi@hotelquickly.com>
2018-01-28 11:36:55 +0700

    Removed unneeded code from Network module

--- app/src/main/java/omise/charity/app/dagger/NetworkModule.java
+++ app/src/main/java/omise/charity/app/dagger/NetworkModule.java
@@ -11,34 +11,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class NetworkModule {
-//	@Singleton
-//	@Provides
-//	OkHttpClient providesHttpClient() {
-//		OkHttpClient.Builder httpClient =
-//				new OkHttpClient.Builder();
-//		httpClient.addInterceptor(new Interceptor() {
-//			@Override
-//			public Response intercept(@NonNull Chain chain) throws IOException {
-//				Request original = chain.request();
-//				HttpUrl originalHttpUrl = original.url();
-//
-//				HttpUrl url = originalHttpUrl.newBuilder()
-////						.addQueryParameter("APPID", Constants.OPEN_WEATHER_KEY)
-//						.addQueryParameter("units","metric")
-//						.build();
-//
-//				// Request customization: add request headers
-//				Request.Builder requestBuilder = original.newBuilder()
-//						.url(url);
-//
-//				Request request = requestBuilder.build();
-//				return chain.proceed(request);
-//			}
-//		});
-//		httpClient.addInterceptor(new HttpLoggingInterceptor()
-//				.setLevel(HttpLoggingInterceptor.Level.BODY));
-//		return httpClient.build();
-//	}
 
 	@Provides
 	@Singleton
